9f3d9d98e9366846edcf369a147d9e7348f7e8ec
Keerthi-143 <44571202+Keerthi-143@users.noreply.github.com>
2018-10-29 17:34:44 +0530

    Add files via upload

---
+++ plans.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title><center>Post Paid Plans</center></title>
+</head>
+<?php
+<body>
+<form><input type="text" value="plans.php">
+Plan Name:<input type="text" name="plan name">
+Monthly Rental:<input type="text" name="monthly rental">
+Free Internet:<input type="text" name="free internet">
+Free Calls:<input type="text" name="free internet">
+Free SMSs:<input type="text" name="free smss">
+Call Charges:<input type="text" name="call charges">
+SMS Charges:<input type="text" name="sms charges">
+Data Charges:<input type="text" name="data charges">
+Roaming Charges:<input type="text" name="roaming charges">
+<input type="submit" value="add">
+</form>
+?>
+</body>
+</html>
+
---
+++ telecom.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Telecom Operator Apllication</title>
+</head>
+<?php
+<body>
+<input type="button" value="prepaid" ClickOn="plan.php">
+<input type="button" value="postpaid" ClickOn="plans.php">
+<input type="submit" value="add">
+?>
+</body>
+</html>
